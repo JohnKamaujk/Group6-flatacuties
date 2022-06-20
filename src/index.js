b59b545ae28c1fb78a1bbfd99629608a4953b326
rahimedu <edwin.duche82@gmail.com>
2022-06-20 14:30:27 -0400

    message

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 const characterName = document.getElementById("name");
   characterName.innerText = character.name;
+  const characterImage = document.getElementById("image");
+  characterImage.src = character.image;
