d7a23c93073d8992aa07a928867df53e4b3fd0c4
rahimedu <edwin.duche82@gmail.com>
2022-06-20 14:40:33 -0400

    message

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ const characterName = document.getElementById("name");
   characterName.innerText = character.name;
   const characterImage = document.getElementById("image");
   characterImage.src = character.image;
+  const currentVotes = document.getElementById("vote-count");
+  currentVotes.innerText = character.votes;
