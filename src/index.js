7d3786e16e9442078bba0631559ad66d81d1a5a5
James Ochieng Obuya <jamesobuyas@gmail.com>
2022-06-21 14:19:18 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     const navBar = document.getElementById("character-bar");
+    function getCharacterDetails() {
+        return fetch("http://localhost:3000/characters")
