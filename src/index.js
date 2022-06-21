aa036562591ab9fd9ad5220087f7f03699899d86
JohnKamaujk <johnjkkamau@gmail.com>
2022-06-21 15:16:14 +0300

    Created an instance of the input and image value

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const newCharacter = document.getElementById("character-form");
 newCharacter.addEventListener("submit", (e) => {
-
-
-
-
-
-
-
-
-    
-})
+  e.preventDefault();
+  const newCharacterName = document.getElementById("name2").value;
+  const newCharacterImage = document.getElementById("image-url").value;
+  const addedCharacter = document.createElement("span");
+  addedCharacter.style.cursor = "pointer";
+  addedCharacter.innerText = newCharacterName;
+});
